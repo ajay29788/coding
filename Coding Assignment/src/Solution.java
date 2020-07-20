da02c58a040632e0216d31aabc4fa8b3060efb5d
ajay29788 <68529079+ajay29788@users.noreply.github.com>
2020-07-20 11:19:57 +0530

    Add files via upload

---
+++ Coding Assignment/src/Solution.java
@@ -0,0 +1,50 @@
+import java.util.*;
+
+public class Solution {
+	
+	static long minimumCost(long price[], int n)
+	{
+		Arrays.sort(price);
+		
+		long totalCost = 0;
+		
+		for (int i = n-1; i > 1; i -= 2)
+		{
+			if(i == 2)
+			{
+				totalCost += price[2] + price[0]; 
+			}
+			 else
+	            { 
+	      
+	                long price_first = price[i] + price[0] + 2 * price[1]; 
+	                long price_second = price[i] + price[i - 1] + 2 * price[0]; 
+	                totalCost += Math.min(price_first, price_second); 
+	            } 
+		}
+		 if (n == 1) 
+	        { 
+	            totalCost += price[0]; 
+	        } 
+	        else
+	        { 
+	            totalCost += price[1]; 
+	        } 
+	      
+	        return totalCost; 
+	    } 
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		long price[] = {300,400,600,700};
+		
+        int n = price.length; 
+      
+        System.out.println(minimumCost(price, n));
+		
+		
+
+	}
+
+}
